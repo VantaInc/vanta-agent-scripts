ed24bc3d0e328694d35152932b84ecb5b1b81b51
Robbie Ostrow <ostrowr@users.noreply.github.com>
2020-03-16 17:44:11 +0000

    upgrade scripts to install v1.3.1 (#15)

--- install-vanta.bat
+++ install-vanta.bat
@@ -4,12 +4,11 @@ IF "%~2"=="" goto :needparams
 IF NOT "%~3"=="" goto :needparams
 
 :: download Vanta
-curl -o vanta-installer.exe https://vanta-agent.s3.amazonaws.com/v0.2.0/vanta-installer.exe
+curl -o vanta-installer.exe https://vanta-agent.s3.amazonaws.com/v1.3.1/vanta-installer.exe
 
 :: write config files
 mkdir C:\ProgramData\Vanta
-echo {"AGENT_KEY": "%1", "NEEDS_OWNER": true, "OWNER_EMAIL": "%2"} > C:\ProgramData\Vanta\enroll_secret.txt
-copy C:\ProgramData\Vanta\enroll_secret.txt C:\ProgramData\Vanta\vanta.conf
+echo {"AGENT_KEY": "%1", "NEEDS_OWNER": true, "OWNER_EMAIL": "%2"} > C:\ProgramData\Vanta\vanta.conf
 
 :: set permissions on config files so installer doesn't overwrite
 icacls C:\ProgramData\Vanta\enroll_secret.txt /grant Everyone:R
@@ -19,10 +18,6 @@ icacls C:\ProgramData\Vanta\vanta.conf /deny Everyone:W
 
 :: install vanta
 vanta-installer.exe /S
-
-:: remove permissions on Everyone
-icacls C:\ProgramData\Vanta\enroll_secret.txt /remove Everyone
-icacls C:\ProgramData\Vanta\vanta.conf /remove Everyone
 
 EXIT
 
