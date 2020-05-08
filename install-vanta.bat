338b53d66c97d7df497f47ca2fdb97bfc98ab0f1
Robbie Ostrow <ostrowr@users.noreply.github.com>
2020-05-08 11:03:14 -0700

    make windows install script more robust (#22)

--- install-vanta.bat
+++ install-vanta.bat
@@ -1,3 +1,4 @@
+@echo off
 :: check to make sure that both AGENT_KEY and OWNER_EMAIL are specified
 IF "%~1"=="" goto :needparams
 IF "%~2"=="" goto :needparams
@@ -8,16 +9,15 @@ curl -o vanta-installer.exe https://vanta-agent.s3.amazonaws.com/v1.5.0/vanta-in
 
 :: write config files
 mkdir C:\ProgramData\Vanta
-echo {"AGENT_KEY": "%1", "NEEDS_OWNER": true, "OWNER_EMAIL": "%2"} > C:\ProgramData\Vanta\vanta.conf
 
 :: set permissions on config files so installer doesn't overwrite
-icacls C:\ProgramData\Vanta\enroll_secret.txt /grant Everyone:R
-icacls C:\ProgramData\Vanta\vanta.conf /grant Everyone:R
-icacls C:\ProgramData\Vanta\enroll_secret.txt /deny Everyone:W
-icacls C:\ProgramData\Vanta\vanta.conf /deny Everyone:W
+icacls C:\ProgramData\Vanta\vanta.conf /inheritance:r
+icacls C:\ProgramData\Vanta\vanta.conf /grant "everyone":R
 
 :: install vanta
 vanta-installer.exe /S
+echo {"AGENT_KEY": "%1", "NEEDS_OWNER": true, "OWNER_EMAIL": "%2"} > C:\ProgramData\Vanta\vanta.conf
+C:\ProgramData\Vanta\vanta-cli reset
 
 EXIT
 
