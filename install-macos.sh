30127b4cdbdf578779b846fd97e418afaff2f9eb
David Zhu <hdavidzhu@gmail.com>
2022-01-18 07:43:50 -0600

    Upgrade to 2.0.8

--- install-macos.sh
+++ install-macos.sh
@@ -5,7 +5,7 @@ set -e
 # VANTA_KEY (the Vanta per-domain secret key)
 # VANTA_OWNER_EMAIL (the email of the person who owns this computer. Ignored if VANTA_KEY is missing.)
 
-PKG_URL="https://vanta-agent-repo.s3.amazonaws.com/targets/versions/2.0.3/vanta-universal.pkg"
+PKG_URL="https://vanta-agent-repo.s3.amazonaws.com/targets/versions/2.0.8/vanta-universal.pkg"
 # Checksum needs to be updated when PKG_URL is updated.
 CHECKSUM="9b164c4719640feec4ca69a340c4d00c8434370248f5ca22bddbf431cf50b781"
 PKG_PATH="$(mktemp -d)/vanta.pkg"
