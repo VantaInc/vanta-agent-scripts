c221efeddf892859b1c495932b1aed5a94525c0b
Scott Bezek (Vanta) <103462496+scottvanta@users.noreply.github.com>
2024-08-08 10:16:41 -0700

    Update to 2.9.0 release (#73)

--- install-macos.sh
+++ install-macos.sh
@@ -6,9 +6,9 @@ set -e
 # VANTA_OWNER_EMAIL (the email of the person who owns this computer. Ignored if VANTA_KEY is missing.)
 # VANTA_REGION (the region the Agent talks to, such as "us" or "eu".)
 
-PKG_URL="https://vanta-agent-repo.s3.amazonaws.com/targets/versions/2.8.1/vanta-universal.pkg"
+PKG_URL="https://vanta-agent-repo.s3.amazonaws.com/targets/versions/2.9.0/vanta-universal.pkg"
 # Checksum needs to be updated when PKG_URL is updated.
-CHECKSUM="31159006453a4352c41eaeab9bbd527fa9af00cdaf5268e06c6def20f52ef7cf"
+CHECKSUM="8f92149bfdc26e29815ff8c46e2c95424c538a096742e444d7feb1fbafaa0aa8"
 DEVELOPER_ID="Vanta Inc (632L25QNV4)"
 CERT_SHA_FINGERPRINT="D90D17FA20360BC635BC1A59B9FA5C6F9C9C2D4915711E4E0C182AA11E772BEF"
 PKG_PATH="$(mktemp -d)/vanta.pkg"
