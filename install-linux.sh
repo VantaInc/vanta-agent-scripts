06110a9f514c1cf26124736621e1b067f79b14bc
SW386 <kevin.wen@vanta.com>
2026-05-20 18:33:43 -0400

    Update default version

--- install-linux.sh
+++ install-linux.sh
@@ -8,7 +8,7 @@
 
 set -e
 
-DEB_URL="https://agent-downloads.vanta.com/targets/versions/2.16.1/vanta-amd64.deb"
+DEB_URL="https://agent-downloads.vanta.com/targets/versions/2.17.2/vanta-amd64.deb"
 # Checksums need to be updated when DEB_URL is updated.
 DEB_CHECKSUM="bb8eccb929b63dc7c7137fd35716a69d5e20b616ae200a89ae519c6d2d6cf7f1"
 DEB_PATH="$(mktemp -d)/vanta.deb"
--- install-macos.sh
+++ install-macos.sh
@@ -6,7 +6,7 @@ set -e
 # VANTA_OWNER_EMAIL (the email of the person who owns this computer)
 # VANTA_REGION (the region Vanta Device Monitor talks to, such as "us", "eu" or "aus".)
 
-PKG_URL="https://agent-downloads.vanta.com/targets/versions/2.16.1/vanta-universal.pkg"
+PKG_URL="https://agent-downloads.vanta.com/targets/versions/2.17.2/vanta-universal.pkg"
 # Checksum needs to be updated when PKG_URL is updated.
 CHECKSUM="66c71af348441c7efdb88d98fd43e8f9d401bd21976bcb0ff7b1a4fb56a9628c"
 DEVELOPER_ID="Vanta Inc (632L25QNV4)"
